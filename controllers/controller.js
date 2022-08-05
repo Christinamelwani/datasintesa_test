8adfcf57441b93b22f1f6a19ca3722ee4ceefad5
Christinamelwani <christinarajm@gmail.com>
2022-08-05 18:47:58 +0700

    Server seems to be done

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,21 +1,87 @@
 const { raw_data } = require("../models/raw_data");
-const csv = require("fast-csv");
+const csv = require("csv-parser");
+const zlib = require("zlib");
 const fs = require("fs");
 class Controller {
   static async uploadFile(req, res, next) {
-    const fileRows = [];
-    csv
-      .parseFile(req.file.path)
-      .on("data", function (data) {
-        fileRows.push(data);
-      })
-      .on("end", function () {
-        console.log(fileRows);
-        fs.unlinkSync(req.file.path);
-      });
+    try {
+      let line = 0;
+      let insertedData = 0;
+      let duplicatedData = 0;
+      fs.createReadStream(req.file.path)
+        .pipe(zlib.createUnzip())
+        .on("error", function () {
+          res.status(400).json({ status: "error", message: "Invalid file" });
+        })
+        .pipe(csv())
+        .on("error", function () {
+          res.status(400).json({ status: "error", message: "Invalid file" });
+        })
+        .on("data", async function (el) {
+          try {
+            if (line !== 0) {
+              const resultTime = new Date(el["Result Time"]);
+              let enodebId = el["Object Name"].substring(
+                el["Object Name"].indexOf("eNodeB ID=") + 10
+              );
+              enodebId = enodebId.substring(0, enodebId.indexOf(","));
+              let cellId = el["Object Name"].substring(
+                el["Object Name"].indexOf("Local Cell ID=") + 14
+              );
+              cellId = cellId.substring(0, cellId.indexOf(","));
+              let availDur = +el["L.Cell.Avail.Dur"];
+
+              const toInsert = {
+                resultTime,
+                enodebId,
+                cellId,
+                availDur,
+              };
+              await raw_data.create(toInsert);
+              insertedData++;
+            }
+          } catch (err) {
+            duplicatedData++;
+          }
+          line++;
+        })
+        .on("end", async function () {
+          res.status(200).json({
+            status: "Success",
+            message:
+              "Data successfully inserted, but might take some time to update",
+          });
+        });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
   static async getGraph(req, res, next) {
-    res.send("get graph works");
+    try {
+      let output = [];
+      const { enodebId, cellId, startDate, endDate } = req.query;
+      if (!enodebId || !cellId || !startDate || !endDate) {
+        throw new Error(
+          `EnodebId, cellId, startDate and endDate must all be provided!`
+        );
+      }
+      const data = await raw_data.filter({
+        enodebId,
+        cellId,
+        startDate,
+        endDate,
+      });
+      data.forEach((el) => {
+        output.push({
+          resultTime: el.resultTime,
+          availability: (el.availDur / 900) * 100,
+        });
+      });
+      res.send(output);
+    } catch (err) {
+      res.status(400).json({ status: "error", message: err.message });
+    }
   }
 }
 
--- models/raw_data.js
+++ models/raw_data.js
@@ -1,48 +1,46 @@
 const { getDatabase } = require("../config/mongoConnection");
-const { ObjectId } = require("mongodb");
+const {} = require("mongodb");
 
 class raw_data {
-  //   static async findAll() {
-  //     try {
-  //       const db = getDatabase();
-  //       const users = db.collection("users");
-  //       const result = await users.find().toArray();
-  //       return result;
-  //     } catch (err) {
-  //       throw err;
-  //     }
-  //   }
-  //   static async findById(id) {
-  //     try {
-  //       const db = getDatabase();
-  //       const users = db.collection("users");
-  //       const result = await users.findOne({ _id: ObjectId(id) });
-  //       if (result === null) {
-  //         throw {
-  //           name: "not found!",
-  //           message: `User with id ${id} does not seem to exist`,
-  //         };
-  //       }
-  //       return result;
-  //     } catch (err) {
-  //       throw err;
-  //     }
-  //   }
-  //   static async create(userData) {
-  //     try {
-  //       const db = getDatabase();
-  //       const users = db.collection("users");
-  //       const dataToInsert = await validate(userData, users);
-  //       dataToInsert.password = createHash(dataToInsert.password);
-  //       const result = await users.insertOne(dataToInsert);
-  //       return {
-  //         id: result.insertedId,
-  //         email: userData.email,
-  //       };
-  //     } catch (err) {
-  //       throw err;
-  //     }
-  //   }
+  static async findAll() {
+    try {
+      const db = getDatabase();
+      const raw_data = db.collection("raw_data");
+      const result = await raw_data.find().toArray();
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+  static async filter({ enodebId, cellId, startDate, endDate }) {
+    try {
+      const db = getDatabase();
+      const raw_data = db.collection("raw_data");
+      const result = await raw_data
+        .find({
+          enodebId,
+          cellId,
+          resultTime: { $gte: new Date(startDate), $lt: new Date(endDate) },
+        })
+        .toArray();
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+  static async create(dataToInsert) {
+    try {
+      const db = getDatabase();
+      const users = db.collection("raw_data");
+      const result = await users.insertOne(dataToInsert);
+      return {
+        id: result.insertedId,
+        resultTime: result.resultTime,
+      };
+    } catch (err) {
+      throw err;
+    }
+  }
   //   static async delete(id) {
   //     try {
   //       const db = getDatabase();
