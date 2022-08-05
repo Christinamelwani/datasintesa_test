cd4d2264e38bc9dfea9b0a03dd332f11c15d52c3
Christinamelwani <christinarajm@gmail.com>
2022-08-05 14:23:00 +0700

    Fix upload

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,16 +1,17 @@
 const { raw_data } = require("../models/raw_data");
 const csv = require("fast-csv");
+const fs = require("fs");
 class Controller {
   static async uploadFile(req, res, next) {
     const fileRows = [];
-
     csv
       .parseFile(req.file.path)
       .on("data", function (data) {
-        fileRows.push(data); // push each row
+        fileRows.push(data);
       })
       .on("end", function () {
         console.log(fileRows);
+        fs.unlinkSync(req.file.path);
       });
   }
   static async getGraph(req, res, next) {
